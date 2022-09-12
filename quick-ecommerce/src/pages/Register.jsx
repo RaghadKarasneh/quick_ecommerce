454c7cd31a72f998d9a7a0ee1442b2da2758bd3a
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-09-12 03:06:56 +0300

    QUICK E-COMMERCE

--- quick-ecommerce/src/pages/Register.jsx
+++ quick-ecommerce/src/pages/Register.jsx
@@ -1,169 +1,195 @@
 import React, { Component } from "react";
-
+import styled from "styled-components";
 import axios from 'axios';
-
+import { mobile } from "../responsive";
+import {  Link } from 'react-router-dom';
 // import "./reg.css";
 
 const regExp = RegExp(
-    /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/
+  /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/
 )
-const passRegex= RegExp(
-    /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+const passRegex = RegExp(
+  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
 )
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+  ${mobile({ width: "75%" })}
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
 
 const formValid = ({ isError, ...rest }) => {
-    let isValid = true;
+  let isValid = true;
 
-    Object.values(rest).forEach(val => {
-        if (val === '') {
-            isValid = false
-        }
-    });
+  Object.values(rest).forEach(val => {
+    if (val === '') {
+      isValid = false
+    }
+  });
 
-    Object.values(isError).forEach(val => {
-        if (val.length > 0) {
-            isValid = false
-        }
-    });
+  Object.values(isError).forEach(val => {
+    if (val.length > 0) {
+      isValid = false
+    }
+  });
 
-    return isValid;
+  return isValid;
 };
 
 export default class UserForm extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            isError: {
-                name: '',
-                email: '',
-                password: '',
-            }
-        }
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      isError: {
+        name: '',
+        email: '',
+        password: '',
+      }
     }
+  }
 
 
-    onSubmit = e => {
-        e.preventDefault();
+  onSubmit = e => {
+    e.preventDefault();
 
-        if (formValid(this.state)) {
-            console.log("form Valid");
-            let user_name=this.state.name
-            let email=this.state.email
-            let password=this.state.password
+    if (formValid(this.state)) {
+      console.log("form Valid");
+      let user_name = this.state.name
+      let email = this.state.email
+      let password = this.state.password
 
 
 
-          axios.post('http://localhost/Quick_Ecommerce/quick_ecommerce/quick-ecommerce/API/register.php?name='+user_name+'&email='+email+'&password='+password);
+      axios.post('http://localhost/Quick_Ecommerce/quick_ecommerce/quick-ecommerce/API/register.php?name=' + user_name + '&email=' + email + '&password=' + password);
 
-          window.location.href = "/Login";
+      window.location.href = "/Login";
 
-            
-        } else {
-            console.log("Form is invalid!");
-            
-        }
-    };
 
-    formValChange = e => {
-        e.preventDefault();
-        const { name, value } = e.target;
-        let isError = { ...this.state.isError };
+    } else {
+      console.log("Form is invalid!");
 
-        switch (name) {
-            case "name":
-                isError.name =
-                    value.length < 4 ? "Atleast 4 characaters required" : "";
-                break;
-
-            case "email":
-                isError.email = regExp.test(value)
-                    ? ""
-                    : "Email address is invalid";
-                break;
-
-            case "password":
-                isError.password = passRegex.test(value)
-                    ? "" 
-                    : "Minimum eight characters, at least one letter and one number";
-                break;
-                
-            default:
-                break;
-                
-        }
-        this.setState({
-            isError,
-            [name]: value
-        })
-    };
-
-    render() {
-      console.log("hello regester");
-        const { isError } = this.state;
-
-        return (
-          <div className="forms" > 
-            <form id='regForm' className="d" onSubmit={this.onSubmit} noValidate>
-              <h1>Sign Up</h1>
-                <div className="form-group">
-                  <div className="form2">
-                    <label>Name</label>
-                    <input
-                        type="text"
-                        className={isError.name.length > 0 ? "is-invalid form-control regForm" : "form-control regForm"}
-                        name="name"
-                        onChange={this.formValChange}
-                    />
-                    {isError.name.length > 0 && (
-                        <span className="invalid-feedback">{isError.name}</span>
-                    )}
-                </div>
-
-                <div className="form-group">
-                    <label>Email</label>
-                    <input
-                        type="email"
-                        className={isError.email.length > 0 ? "is-invalid form-control regForm" : "form-control regForm"}
-                        name="email"
-                        onChange={this.formValChange}
-                    />
-                    {isError.email.length > 0 && (
-                        <span className="invalid-feedback">{isError.email}</span>
-                    )}
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        className={isError.password.length > 0 ? "is-invalid form-control regForm" : "form-control regForm"}
-                        name="password"
-                        onChange={this.formValChange}
-                    />
-                    {isError.password.length > 0 && (
-                        <span className="invalid-feedback">{isError.password}</span>
-                    )}
-                </div>
-
-                <div class="BTNcontainer">
-                <div class="center">
-                <button type="submit" className="btn " id="regBtn">Sign Up </button>
-                </div>
-                </div>
-                <p id='regP'>Already have an account? <a href="/Login">Login</a></p>
-                
-                </div>
-                
-
-            </form>
-            
-            </div>
-            
-           
-        );
     }
+  };
+
+  formValChange = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let isError = { ...this.state.isError };
+
+    switch (name) {
+      case "name":
+        isError.name =
+          value.length < 4 ? "Atleast 4 characaters required" : "";
+        break;
+
+      case "email":
+        isError.email = regExp.test(value)
+          ? ""
+          : "Email address is invalid";
+        break;
+
+      case "password":
+        isError.password = passRegex.test(value)
+          ? ""
+          : "Minimum eight characters, at least one letter and one number";
+        break;
+
+      default:
+        break;
+
+    }
+    this.setState({
+      isError,
+      [name]: value
+    })
+  };
+
+  render() {
+    console.log("hello regester");
+    const { isError } = this.state;
+
+    return (
+      <Container>
+        <Wrapper>
+          <Title>SIGN UP</Title>
+          <Form className="d" onSubmit={this.onSubmit} noValidate>
+            <Input className={isError.name.length > 0 ? "is-invalid form-control regForm" : "form-control regForm"}
+              name="name"
+              onChange={this.formValChange} placeholder="username" />
+            {isError.name.length > 0 && (
+              <span className="invalid-feedback">{isError.name}</span>
+            )}
+            <Input className={isError.email.length > 0 ? "is-invalid form-control regForm" : "form-control regForm"}
+              name="email"
+              onChange={this.formValChange} placeholder="email" />
+            {isError.email.length > 0 && (
+              <span className="invalid-feedback">{isError.email}</span>
+            )}
+            <Input className={isError.password.length > 0 ? "is-invalid form-control regForm" : "form-control regForm"}
+              name="password"
+              onChange={this.formValChange} placeholder="password" />
+            {isError.password.length > 0 && (
+              <span className="invalid-feedback">{isError.password}</span>
+            )}
+            <Button type="submit" className="btn " id="regBtn">LOGIN</Button>
+            <Link to="/Login">Already have an account?</Link>
+
+          </Form>
+        </Wrapper>
+      </Container>
+
+
+
+
+
+
+    );
+  }
 }
